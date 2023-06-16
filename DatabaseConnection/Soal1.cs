2401dee7d3166dd1ed67f3c75b60960a73cfed18
Kherogen <rayvaldoprawira@gmail.com>
2023-06-16 11:38:42 +0700

    Edit LINQ

---
+++ DatabaseConnection/Soal1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConnection
+{
+    public class Soal1
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Salary { get; set; }
+        public string DName { get; set; }
+        public string LStreet { get; set; }
+        public string CName { get; set; }
+        public string RName { get; set; }
+
+    }
+}
---
+++ DatabaseConnection/Soal2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConnection
+{
+  public class Soal2
+    {
+        public string DName { get; set; }
+        public int TotalEmp { get; set; }
+        public int MinSalary { get; set; }
+        public int MaxSalary { get; set; }
+        public int AvgSalary { get; set; }
+    }
+}
